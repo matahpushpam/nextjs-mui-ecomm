c544788c9e34604f6dc01987f23e35432449c9d1
Pushpam <matahpushpam@gmail.com>
2023-12-27 22:41:46 +0530

    Added product list page

--- src/app/(products)/product-list/page.tsx
+++ src/app/(products)/product-list/page.tsx
@@ -1,24 +1,53 @@
 'use client';
 
-import Button from '@mui/material/Button';
 import { useEffect, useState } from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 
 export default function ProductList() {
     const [products, setProducts] = useState([]);
     useEffect(() => {
         fetch('/api/products')
-        .then(res => res.json())
-        .then(json => setProducts(json))
-        .catch(err => console.error(err))
+            .then(res => res.json())
+            .then(json => setProducts(json))
+            .catch(err => console.error(err))
     }, [])
     return (
         <>
-        <h1>Hello, Product list Next.js!</h1>
-        <div>
-            {products.map((product: any) => {
-                return <li>{product.id}</li>
-            })}
-        </div>
+            <Box sx={{ flexGrow: 1 }}>
+                <Grid container spacing={5}>
+                    {products.map((product: any) => {
+                        return (
+                            <Grid item xs={3} key={product.id}>
+                                <Card sx={{ maxWidth: 345, maxHeight: 300, overflow: 'scroll' }}>
+                                    <CardMedia
+                                        sx={{ height: 140 }}
+                                        image={product.images[0]}
+                                        title="green iguana"
+                                    />
+                                    <CardActions>
+                                        <Button size="small">Learn More</Button>
+                                    </CardActions>
+                                    <CardContent>
+                                        <Typography gutterBottom variant="h5" component="div">
+                                            {product.title}
+                                        </Typography>
+                                        <Typography variant="body2" color="text.secondary">
+                                            {product.description}
+                                        </Typography>
+                                    </CardContent>
+                                </Card>
+                            </Grid>
+                        )
+                    })}
+                </Grid>
+            </Box>
         </>
     )
 }
